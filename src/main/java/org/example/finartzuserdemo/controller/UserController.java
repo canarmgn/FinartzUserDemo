a98b0dd00ffdedd3d92785141548b01954138cee
ceren <cerennurozdemir92@gmail.com>
2024-05-08 11:40:02 +0300

    removes line

--- src/main/java/org/example/finartzuserdemo/controller/UserController.java
+++ src/main/java/org/example/finartzuserdemo/controller/UserController.java
@@ -18,7 +18,6 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-
     @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
